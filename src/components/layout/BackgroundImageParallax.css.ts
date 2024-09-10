dd326da91ebcd02bcb87803b19380c8f963e82eb
Miche Mayer <miche.mayer@outlook.de>
2024-09-10 16:22:08 +0000

    parallax container

---
+++ src/components/layout/BackgroundImageParallax.css.ts
@@ -0,0 +1,44 @@
+import { style } from "@vanilla-extract/css"
+
+export const fixedBackgroundContainer = style({
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    height: '100%',
+    overflow: 'clip',
+    zIndex: 0,
+})
+
+export const parallaxContainer = style({
+    position: 'relative',
+    '@supports': {
+        'overflow: clip': {
+            height: '2000vh',
+            marginTop: '-1000vh',
+        }
+    },
+})
+
+/*
+* Versionen < 16  von Safari untersttützen kein overflow-clip.
+* Um Overflow am Seitenende zu vermeiden wird die Containerhöhe in diesen Browser Versionen limitiert.
+* Damit wird der Parallax-Effekt deaktiviert.
+*/
+export const fallbackContainer = style({
+    height: '100%',
+    overflow: 'hidden',
+})
+
+export const backgroundImage = style({
+    position: 'sticky',
+    top: 0,
+    width: '100%',
+    objectFit: 'cover',
+})
+
+export const contentContainer = style({
+    position: 'relative',
+    zIndex: 1,
+    width: '100%',
+    overflowX: 'hidden',
+})
