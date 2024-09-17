0dc525ccc929af85d95f5a4772d5f6c09c5c5af3
Miche Mayer <miche.mayer@outlook.de>
2024-09-17 21:47:34 +0000

    carve out basic intro screen layout

--- src/components/content/intro/IntroScreen.css.ts
+++ src/components/content/intro/IntroScreen.css.ts
@@ -5,6 +5,31 @@ export const fullscreen = sprinkles({
     height: 'screen',
 })
 
+export const backgroundVideo = style({
+    position: 'relative',
+    width: '100%',
+    height: '100vh',
+    objectFit: 'cover',
+    filter: 'brightness(0.3)',
+})
+
 export const typography = style({
-    color: vars.colors.textLight,
+    color: vars.colors.white,
+})
+
+export const split = sprinkles({
+    display: 'flex',
+    flexDirection: { xs: 'column', md: 'row' },
+    gap: { xs: 'lg', md: '2xl' },
+})
+
+export const heading = style({
+    width: '100%',
+    textAlign: 'center',
+    marginTop: '5rem',
+    marginBottom: '5rem',
+    fontSize: '6rem',
+    fontWeight: 700,
+    lineHeight: 1.2,
+    fontFamily: vars.typography.fonts.secondaryFontFamily,
 })
---
+++ src/components/layout/Card.css.ts
@@ -0,0 +1,9 @@
+import { style } from '@vanilla-extract/css'
+
+export const flexBox = style({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '1rem',
+})
