8fe6eb3a3432a9da7cc52f969aa59d4c24a7b2ed
Miche Mayer <miche.mayer@outlook.de>
2024-10-22 09:07:06 +0000

    color social links

--- src/components/navigation/socials/SocialLink.css.ts
+++ src/components/navigation/socials/SocialLink.css.ts
@@ -1,17 +1,37 @@
-import { sprinkles } from '@styles';
+import { sprinkles, vars, type Color } from '@styles'
 import { style } from '@vanilla-extract/css'
+import { recipe } from '@vanilla-extract/recipes'
 
-export const container = style([
-    {},
+const container =
     sprinkles({
         display: 'flex',
         flexDirection: 'row',
         alignItems: 'center',
         gap: 'xs',
-    }),
-])
+    })
+
+export const colorVariants = Object.entries(vars.colors)
+    .reduce(
+        (colorDictionary, [key, value]) => ({
+            ...colorDictionary,
+            [key]: { color: value, fill: value },
+        }),
+        {} as Record<Color, { color: string, fill: string }>
+    )
+
+export type SocialLinkColor = keyof typeof colorVariants
 
 export const icon = style({
     width: '1.5em',
     height: '1.5em',
+})
+
+export const socialLink = recipe({
+    base: container,
+    variants: {
+        color: {
+            ...colorVariants,
+        },
+    },
+    defaultVariants: { color: 'secondaryLight' },
 })
--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 export const SITE_TITLE = 'Jazz of Course'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_DESCRIPTION = 'Willkommen auf unserer Webseite!'
 export const BAND_NAME = 'Jazz of Course'
